2a9a3c013db1aa36368c6eef959ce9ea4e087237
thong <ttthong.dut@gmail.com>
2021-05-02 23:30:26 +0700

    Initial commit; The application has had CRUD features already with simple style

---
+++ vulnblogapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VulnblogappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vulnblogapp'
---
+++ vulnblogapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+# View for Home
+class HomeView(ListView):
+    model = Post
+    template_name = "home.html"
+    ordering = ['-post_date']
+
+
+# View for Article details
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = "article_details.html"
+
+# View for adding posts
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__'
+
+# View for update post
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title','title_tag','body']
+
+
+# View for delete post
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
