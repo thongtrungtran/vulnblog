80d1a9936daa2d583ca0f404ffa6beb9ce03c85f
thong <ttthong.dut@gmail.com>
2021-05-03 23:25:38 +0700

    created app 'members' for authentication

--- vulnblog/settings.py
+++ vulnblog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vulnblogapp'
+    'vulnblogapp',
+    'members'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
